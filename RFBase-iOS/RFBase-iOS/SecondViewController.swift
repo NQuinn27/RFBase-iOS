33b99b0e8f76e687119d3729da970f27504a9b4a
Niall Quinn <niall@niallquinn.me>
2017-04-14 13:01:13 +0100

    Migrate iOS

---
+++ RFBase-iOS/RFBase-iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RFBase-iOS
+//
+//  Created by niall quinn on 14/04/2017.
+//  Copyright © 2017 niall quinn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
